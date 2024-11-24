222a811dc547581e9c1ee3e0780ae4e2c8d2ce7d
RIshabh Rai <rishabh@euracle.com>
2024-11-24 10:59:22 +0530

    handled cors

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const connectDB = require('./config/db');
 const aiRoutes = require('./routes/aiRoutes');
+const cors = require('cors')
 
 require('dotenv').config();
 
 const app = express();
 
 // Middleware
+app.use(cors()); // Enable CORS for all routes
 app.use(express.json());
 
 // Connect to DB
