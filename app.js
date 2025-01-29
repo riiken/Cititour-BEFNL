69c78b46e153ad93b0e044cb9ac199b6a56fba67
RIshabh Rai <rai22160@gmail.com>
2025-01-29 22:21:04 +0530

    Removed cors for now

--- app.js
+++ app.js
@@ -1,50 +1,30 @@
 const express = require('express');
 const connectDB = require('./config/db');
 const aiRoutes = require('./routes/aiRoutes');
-const cors = require('cors');
+const cors = require('cors')
 
 require('dotenv').config();
 
 const app = express();
 
 const allowedOrigins = [
-    'http://localhost:4200',          // Angular Development
-    'https://cititour.onrender.com',  // Deployed Web App
-    'capacitor://localhost',          // Android/iOS App
-    'http://localhost'                // Localhost for mobile testing
-];
+    'http://localhost:4200',
+    'https://cititour.onrender.com'
+  ];
 
-const corsOptions = {
+  const corsOptions = {
     origin: function (origin, callback) {
-        console.log("Incoming Request Origin:", origin);
-
-        if (!origin) {
-            // Allow if request is from the Capacitor app (which has no origin)
-            return callback(null, true);
-        }
-
-        if (allowedOrigins.includes(origin)) {
-            return callback(null, true);
-        } 
-        
-        return callback(new Error('Not allowed by CORS'));
-    }
-};
-
+        console.log(origin);
+      if (allowedOrigins.indexOf(origin) !== -1) { // !origin for testing in Postman
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+  };
 // Middleware
-app.use(cors(corsOptions)); // Enable CORS for all routes
+app.use(cors()); // Enable CORS for all routes
 app.use(express.json());
-
-// Middleware to allow only requests from Capacitor app (checks User-Agent)
-app.use((req, res, next) => {
-    const userAgent = req.headers['user-agent'] || '';
-
-    if (!req.headers.origin && userAgent.includes('Capacitor')) {
-        return next();
-    }
-
-    next();
-});
 
 // Connect to DB
 connectDB();
